d6b98c53de3db5efa93e85fba04110f318934356
tlebon <tlebon@gmail.com>
2018-09-20 18:12:27 +0200

    reactstrap navbar implemented

--- src/app/js/Application.jsx
+++ src/app/js/Application.jsx
@@ -16,77 +16,87 @@ class Application extends React.Component {
     super(props);
 
     this.state = {
-      user: this._setUser(true)
+      user: this._setUser(true),
+      isOpen: false
     };
 
     this._setUser = this._setUser.bind(this);
     this._resetUser = this._resetUser.bind(this);
+    this._toggle = this._toggle.bind(this);
   }
 
+
+  
   componentDidMount() {
     this._setUser();
   }
+      _toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+      }
 
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Navigation user={this.state.user} />
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => <Home user={this.state.user} />}
-            />
-            <Route
-              exact
-              path="/discover"
-              render={() => <Discover user={this.state.user} />}
-            />
-            <Route
-              exact
-              path="/profile"
-              render={() => (
-                <Profile user={this.state.user} setUser={this._setUser} />
-              )}
-            />
-            <Route
-              exact
-              path="/profile/:username"
-              render={() => (
-                <Profile user={this.state.user} setUser={this._setUser} />
-              )}
-            />
-            <Route
-              path="/auth"
-              render={() => (
-                <Auth setUser={this._setUser} resetUser={this._resetUser} />
-              )}
-            />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
 
-  _resetUser() {
-    this.setState({
-      user: null
-    });
-  }
+render() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navigation user={this.state.user} open={this.state.isOpen} toggle={this._toggle} />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <Home user={this.state.user} />}
+          />
+          <Route
+            exact
+            path="/discover"
+            render={() => <Discover user={this.state.user} />}
+          />
+          <Route
+            exact
+            path="/profile"
+            render={() => (
+              <Profile user={this.state.user} setUser={this._setUser} />
+            )}
+          />
+          <Route
+            exact
+            path="/profile/:username"
+            render={() => (
+              <Profile user={this.state.user} setUser={this._setUser} />
+            )}
+          />
+          <Route
+            path="/auth"
+            render={() => (
+              <Auth setUser={this._setUser} resetUser={this._resetUser} />
+            )}
+          />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
 
-  _setUser(init) {
-    const token = localStorage.getItem("identity");
-    if (token) {
-      const decoded = jwtDecode(token);
-      delete decoded.iat;
-      if (init) return decoded;
-      this.setState({ user: decoded });
-    } else {
-      return null;
-    }
+_resetUser() {
+  this.setState({
+    user: null
+  });
+}
+
+_setUser(init) {
+  const token = localStorage.getItem("identity");
+  if (token) {
+    const decoded = jwtDecode(token);
+    delete decoded.iat;
+    if (init) return decoded;
+    this.setState({ user: decoded });
+  } else {
+    return null;
   }
+}
 }
 
 export default Application;
