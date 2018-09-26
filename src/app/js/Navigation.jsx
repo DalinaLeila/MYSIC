a05f72c4c44ff0b350cbe334129b4c17d9163a81
tlebon <tlebon@gmail.com>
2018-09-26 15:45:44 +0200

    overflow for notifications

--- src/app/js/Navigation.jsx
+++ src/app/js/Navigation.jsx
@@ -37,7 +37,22 @@ const Navigation = props => {
                       width="40px"
                     />
                   </DropdownToggle>
-                  <DropdownMenu>
+                  <DropdownMenu modifiers={{
+                setMaxHeight: {
+                  enabled: true,
+                  order: 890,
+                  fn: data => {
+                    return {
+                      ...data,
+                      styles: {
+                        ...data.styles,
+                        overflow: "auto",
+                        maxHeight: 600
+                      }
+                    };
+                  }
+                }
+              }}>
                     <DropdownItem>
                       <Notifications />
                     </DropdownItem>
