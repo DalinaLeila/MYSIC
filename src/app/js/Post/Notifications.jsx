d4da8fbc1815e58a2be4d9a95268ac95e6976fa6
Dalina <d.weidinger.16@ucl.ac.uk>
2018-09-27 12:20:00 +0200

    notes profilePicture

--- src/app/js/Post/Notifications.jsx
+++ src/app/js/Post/Notifications.jsx
@@ -4,29 +4,18 @@ import api from "../utils/api";
 import { Link } from "react-router-dom";
 import { DropdownItem } from "reactstrap";
 
-
-
-const Notifications = props =>  {
- 
-
- 
-    let notePosts = props.list.map((post, index) => {
-
-      return (
-        <DropdownItem key={index}>
-          <img
-            src={post.profilePicture}
-            width="50px"
-            className="profilePicture"
-          />
-          <Link to={`profile/${post.othersName}`}> {post.othersName}</Link>{" "}
-          {post.kind}s you
-          {post.postId && "r post"}
-        </DropdownItem>
-      );
-    });
-    return <div>{notePosts}</div>;
-  }
-
+const Notifications = props => {
+  let notePosts = props.list.map((post, index) => {
+    return (
+      <DropdownItem key={index}>
+        <img src={post.profilePicture} className="profilePicture" />
+        <Link to={`profile/${post.othersName}`}> {post.othersName}</Link>{" "}
+        {post.kind}s you
+        {post.postId && "r post"}
+      </DropdownItem>
+    );
+  });
+  return <div>{notePosts}</div>;
+};
 
 export default Notifications;
